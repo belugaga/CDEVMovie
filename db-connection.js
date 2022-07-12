88191d120e84da0b4842a06e1064a28f60fdbf6a
belugaga <109129621+belugaga@users.noreply.github.com>
2022-07-12 15:18:18 +0800

    Update db-connection.js

--- db-connection.js
+++ db-connection.js
@@ -1,9 +1,9 @@
 var mysql = require('mysql');
 var connection = mysql.createConnection({
-    host: 'dbrds1.cjb5vrcyxql7.ap-southeast-1.rds.amazonaws.com',
+    host: 'database-1.cjb5vrcyxql7.ap-southeast-1.rds.amazonaws.com',
     port: '3306',
     user: 'admin',
-    password: 'tG078568',
+    password: 'password',
     database: 'movie_info'
 });
 
